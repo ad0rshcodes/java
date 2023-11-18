c4c68c6be4ece4bc5309c89799e2a9e39da2b80b
ad0rshcodes <adarsharma.me@gmail.com>
2023-11-17 23:44:25 -0600

    Array Questions

--- lectures/08-Array/code/src/ArrayListSyntax.java
+++ lectures/08-Array/code/src/ArrayListSyntax.java
@@ -17,6 +17,14 @@ public class ArrayListSyntax {
         System.out.println(list.get(0));
 
 
+        /*
+        1/ Size of an array list is fixed.
+        2/ Say Arraylist fills by some amount -> it will create a new arraylist of different size.
+        3/ all the elements in the old arraylist would be copied in the new one.
+        4/ All this happens in O(1) time complexity.
+
+         */
+
 
     }
 }
