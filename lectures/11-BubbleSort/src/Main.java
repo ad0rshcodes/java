3d3b513cf9b323940b3ab99d1678a347e908da79
Sharma <sharmaad@eckert.cs.grinnell.edu>
2023-11-23 17:52:12 -0600

    add bubble and selection sort

--- lectures/11-BubbleSort/src/Main.java
+++ lectures/11-BubbleSort/src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    
+    public static void main(String [] args) {
         
+        int [] args2 = {5,4,3,2,1};
+        int [] args3 = {7,8,9,10,11};
+        
+
+        binarySearch(args2);
+        binarySearch(args3);
+
+    }
+
+
+    public static void binarySearch(int [] arr){
+
+        boolean swapped;
+
+        for( int i = 0; i < arr.length - 1; i++){
+
+            swapped = false;
+
+            for(int j = 1; j < arr.length - i ; j++){
+                
+                if (arr[j] < arr [j-1]){
+                    int temp = arr[j];
+
+                    arr [j] = arr [j-1];
+                    arr [j-1] = temp;
+                }
+                swapped = true;
+            }
+
+            if (!swapped){
+                break;
+            }
+
+        }
+
+
+        System.out.println(Arrays.toString(arr));
     }
 }
---
+++ lectures/12-SelectionSort/Selection.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+
+public class Selection{
+    public static void main(String[] args) {
+        
+        int [] arr = {5,44,31,2,18};
+        selection(arr);
+
+    }
+
+
+    public static void selection (int [] arr){
+
+        
+
+        for (int i = 0; i < arr.length; i++){
+
+            int index = 0;
+            int max = Integer.MIN_VALUE;
+
+            for (int j = 0; j < arr.length - i; j++){
+
+                if (arr[j] > max){
+                    max = arr[j];
+                    index = j;
+                }
+            }
+
+            arr[index] = arr[arr.length - i - 1];
+            arr [arr.length - i - 1] = max;
+
+            System.out.println(Arrays.toString(arr));
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
